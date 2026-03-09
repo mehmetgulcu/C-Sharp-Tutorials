2694eed23fee8681947cac1e7784d2909811d6b8
“mehmetgulcu” <mehmetgulcudeveloper@gmail.com>
2026-03-09 21:44:42 +0300

    first commit

---
+++ 1_BasicsOfCsharp/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Merhaba Mehmet!");
+        Console.WriteLine("Dotnet Console uygulaması çalışıyor.");
+    }
+}
